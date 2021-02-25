75351a5dac306436232274a65b736043f179d19f
AntoniPizarro <apizarro@cifpfbmoll.eu>
2021-02-25 13:08:33 +0100

    build: Proyecto maven
    Se ha creado la estructura del proyecto con Maven

---
+++ rails/src/main/java/gpl/pizarro/rails/App.java
@@ -0,0 +1,13 @@
+package gpl.pizarro.rails;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
